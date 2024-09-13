86d02a8595adb1f5b57b9d1f1d21ed94499a2837
Howard <547361821@qq.com>
2024-09-13 14:43:03 +0800

    init

--- 数据结构课程实验答案/1C.cpp
+++ 数据结构课程实验答案/1C.cpp
@@ -1,98 +1,98 @@
-//#include<iostream>  
-//using namespace std;
-//
-//struct ListNode
-//{
-//    int val;
-//    ListNode* next;
-//    ListNode(int x) : val(x), next(nullptr) {}
-//
-//};
-//
-//struct List {
-//    ListNode* head = nullptr;
-//
-//    void insert(int x) {
-//
-//        ListNode* n = new ListNode(x);
-//
-//        if (head == nullptr || head->val >= x) {
-//            n->next = head;
-//            head = n;
-//        }
-//        else {
-//            ListNode* current = head;
-//            while (current->next != nullptr && current->next->val < x) {
-//                current = current->next;
-//            }
-//            n->next = current->next;
-//            current->next = n;
-//        }
-//    }
-//
-//     
-//    void printList() {
-//        ListNode* current = head;   
-//        while (current != nullptr) { 
-//            cout << current->val << " ";
-//            current = current->next;  
-//        }
-//        cout << endl;
-//    }
-//    static ListNode* PlusLists(ListNode* l1, ListNode* l2) {
-//          
-//        ListNode sentinel(0);
-//        ListNode* current = &sentinel;
-//  
-//        while (l1 != nullptr && l2 != nullptr) {
-//            if (l1->val < l2->val) {
-//                current->next = l1;
-//                l1 = l1->next;
-//            }
-//            else {
-//                current->next = l2;
-//                l2 = l2->next;
-//            }
-//            current = current->next;
-//        }
-//
-//         
-//        if (l1 != nullptr) {
-//            current->next = l1;
-//        }
-//        if (l2 != nullptr) {
-//            current->next = l2;
-//        }
-//
-//         
-//        return sentinel.next;
-//    }
-//
-//    void PlusWith(List* other) {
-//        head = PlusLists(head, other->head);
-//    }
-//};
-//
-// 
-//int main() {
-//    int a, b;
-//    while (cin >> a) {
-//        cin >> b;
-//        List* l1 = new List;
-//        for (int i = 0; i < a; i++) {
-//            int temp;
-//            cin >> temp;
-//            l1->insert(temp);
-//        }
-//
-//
-//        List* l2 = new List;
-//        for (int i = 0; i < b; i++) {
-//            int temp;
-//            cin >> temp;
-//            l2->insert(temp);
-//        }
-//        l1->PlusWith(l2);
-//        l1->printList();
-//    }
-//}
+#include<iostream>  
+using namespace std;
+
+struct ListNode
+{
+    int val;
+    ListNode* next;
+    ListNode(int x) : val(x), next(nullptr) {}
+
+};
+
+struct List {
+    ListNode* head = nullptr;
+
+    void insert(int x) {
+
+        ListNode* n = new ListNode(x);
+
+        if (head == nullptr || head->val >= x) {
+            n->next = head;
+            head = n;
+        }
+        else {
+            ListNode* current = head;
+            while (current->next != nullptr && current->next->val < x) {
+                current = current->next;
+            }
+            n->next = current->next;
+            current->next = n;
+        }
+    }
+
+     
+    void printList() {
+        ListNode* current = head;   
+        while (current != nullptr) { 
+            cout << current->val << " ";
+            current = current->next;  
+        }
+        cout << endl;
+    }
+    static ListNode* PlusLists(ListNode* l1, ListNode* l2) {
+          
+        ListNode sentinel(0);
+        ListNode* current = &sentinel;
+  
+        while (l1 != nullptr && l2 != nullptr) {
+            if (l1->val < l2->val) {
+                current->next = l1;
+                l1 = l1->next;
+            }
+            else {
+                current->next = l2;
+                l2 = l2->next;
+            }
+            current = current->next;
+        }
+
+         
+        if (l1 != nullptr) {
+            current->next = l1;
+        }
+        if (l2 != nullptr) {
+            current->next = l2;
+        }
+
+         
+        return sentinel.next;
+    }
+
+    void PlusWith(List* other) {
+        head = PlusLists(head, other->head);
+    }
+};
+
+ 
+int main() {
+    int a, b;
+    while (cin >> a) {
+        cin >> b;
+        List* l1 = new List;
+        for (int i = 0; i < a; i++) {
+            int temp;
+            cin >> temp;
+            l1->insert(temp);
+        }
+
+
+        List* l2 = new List;
+        for (int i = 0; i < b; i++) {
+            int temp;
+            cin >> temp;
+            l2->insert(temp);
+        }
+        l1->PlusWith(l2);
+        l1->printList();
+    }
+}
--- 课程示例代码/最大堆 Maxheap.cpp
+++ 课程示例代码/最大堆 Maxheap.cpp
@@ -1,119 +1,119 @@
-//#include<iostream>  
-//using namespace std;
-//
-//
-//template<class Elem>
-//class Maxheap {
-//private:
-//
-//	Elem* heap;
-//	int size;
-//	int n;
-//
-//public:
-//
-//	Maxheap(Elem* h, int size, int n) {
-//		this->heap = h;
-//		this->size = size;
-//		this->n = n;
-//		build_maxheap();
-//	}
-//
-//	//与其他节点之间的联系
-//	int parent(int pos) {
-//		return (pos - 1) / 2;
-//	}
-//
-//	int leftchild(int pos) {
-//		return 2 * pos + 1;
-//	}
-//
-//	int rightchild(int pos) {
-//		return 2 * pos + 2;
-//	}
-//
-//	//检测自身是否是叶子节点
-//	bool isleaf(int pos) {
-//		return (pos >= n / 2) && (pos < n);
-//	}
-//
-//	//按照最大堆的定义对原来的 pos 位置上的元素进行排序
-//	void sitdown(int pos) {
-//		while (!isleaf(pos)) {
-//			int j = leftchild(pos);
-//			int rc = rightchild(pos);
-//			if (rc < n && heap[j] < heap[rc]) {
-//				j = rc;
-//			}
-//			if (heap[pos] >= heap[j]) {
-//				return;
-//			}
-//			Elem temp = heap[pos];
-//			heap[pos] = heap[j];
-//			heap[j] = temp;
-//			pos = j;
-//		}
-//	}
-//
-//	//对原有的数组 h 进行排列
-//	void build_maxheap() {
-//		for (int i = n / 2; i >= 0; i--) {
-//			sitdown(i);
-//		}
-//		return;
-//	}
-//
-//	//打印输出
-//	void printHeap() {
-//		for (int i = 0; i < n; i++) {
-//			cout << heap[i] << " ";
-//		}
-//		cout << endl;
-//	}
-//
-//	//移除并返回堆顶元素
-//	Elem removetop() {
-//
-//		if (n == 0) {
-//			return false;
-//		}
-//
-//		Elem top = heap[0];
-//		heap[0] = heap[n - 1];  
-//		n--;  
-//		sitdown(0);  
-//
-//		return top; 
-//	}
-//
-//	//移除并返回堆顶元素
-//	bool insert(const Elem& new_item) {
-//		if (n >= size) {
-//			return false;
-//		}
-//
-//		int cur = n++;
-//		heap[cur] = new_item;
-//		while (cur != 0 && heap[cur] > heap[parent(cur)]) {
-//			Elem temp = heap[cur];
-//			heap[cur] = heap[parent(cur)];
-//			heap[parent(cur)] = temp;
-//			cur = parent(cur);
-//		}
-//		return true;
-//	}
-//
-//
-//};
-//
-//
-//int main() {
-//	int h[10] = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
-//	Maxheap<int> heap(h, 20, 10);
-//	heap.printHeap();
-//	heap.removetop();
-//	heap.printHeap();
-//	heap.insert(10);
-//	heap.printHeap();
-//
-//}
+#include<iostream>  
+using namespace std;
+
+
+template<class Elem>
+class Maxheap {
+private:
+
+	Elem* heap;
+	int size;
+	int n;
+
+public:
+
+	Maxheap(Elem* h, int size, int n) {
+		this->heap = h;
+		this->size = size;
+		this->n = n;
+		build_maxheap();
+	}
+
+	//与其他节点之间的联系
+	int parent(int pos) {
+		return (pos - 1) / 2;
+	}
+
+	int leftchild(int pos) {
+		return 2 * pos + 1;
+	}
+
+	int rightchild(int pos) {
+		return 2 * pos + 2;
+	}
+
+	//检测自身是否是叶子节点
+	bool isleaf(int pos) {
+		return (pos >= n / 2) && (pos < n);
+	}
+
+	//按照最大堆的定义对原来的 pos 位置上的元素进行排序
+	void sitdown(int pos) {
+		while (!isleaf(pos)) {
+			int j = leftchild(pos);
+			int rc = rightchild(pos);
+			if (rc < n && heap[j] < heap[rc]) {
+				j = rc;
+			}
+			if (heap[pos] >= heap[j]) {
+				return;
+			}
+			Elem temp = heap[pos];
+			heap[pos] = heap[j];
+			heap[j] = temp;
+			pos = j;
+		}
+	}
+
+	//对原有的数组 h 进行排列
+	void build_maxheap() {
+		for (int i = n / 2; i >= 0; i--) {
+			sitdown(i);
+		}
+		return;
+	}
+
+	//打印输出
+	void printHeap() {
+		for (int i = 0; i < n; i++) {
+			cout << heap[i] << " ";
+		}
+		cout << endl;
+	}
+
+	//移除并返回堆顶元素
+	Elem removetop() {
+
+		if (n == 0) {
+			return false;
+		}
+
+		Elem top = heap[0];
+		heap[0] = heap[n - 1];  
+		n--;  
+		sitdown(0);  
+
+		return top; 
+	}
+
+	//移除并返回堆顶元素
+	bool insert(const Elem& new_item) {
+		if (n >= size) {
+			return false;
+		}
+
+		int cur = n++;
+		heap[cur] = new_item;
+		while (cur != 0 && heap[cur] > heap[parent(cur)]) {
+			Elem temp = heap[cur];
+			heap[cur] = heap[parent(cur)];
+			heap[parent(cur)] = temp;
+			cur = parent(cur);
+		}
+		return true;
+	}
+
+
+};
+
+
+int main() {
+	int h[10] = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+	Maxheap<int> heap(h, 20, 10);
+	heap.printHeap();
+	heap.removetop();
+	heap.printHeap();
+	heap.insert(10);
+	heap.printHeap();
+
+}
